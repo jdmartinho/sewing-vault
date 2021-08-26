9681b42d78d8c42f0c0ee7552fb85c58f62474da
João Martinho <jdmartinho@gmail.com>
2021-08-26 18:57:37 +0100

    Add garment types and dynamic generation of list with remove buttons.

--- app/addnewrenderer.js
+++ app/addnewrenderer.js
@@ -8,6 +8,9 @@ const coverImageDisplay = document.querySelector("#cover-image");
 const openCoverImageButton = document.querySelector("#open-cover-image");
 const companyNameInput = document.querySelector("#company-name");
 const yearInput = document.querySelector("#year");
+const garmentTypeInput = document.querySelector("#garment");
+const addGarmentTypeButton = document.querySelector("#add-garment-type");
+const garmentsAddedDisplay = document.querySelector("#garments-added");
 const notesInput = document.querySelector("#notes");
 const additionalImagesDisplay = document.querySelector("#additional-images");
 const addImagesButton = document.querySelector("#add-images");
@@ -19,12 +22,25 @@ const ADD_NEW_WINDOW_ID = "addnew";
 let COVER_IMAGE;
 // At any given moment this array contains the images to display and save
 let ADDITIONAL_IMAGES_TO_SAVE = [];
+let GARMENT_TYPES_TO_SAVE = new Set();
+garmentTypeInput.value = null;
 
 /***** Event Listeners *****/
 
 openCoverImageButton.addEventListener("click", () => {
   console.log("addnewrenderer - click open cover image button");
   ipcRenderer.send("open-cover-image-button-clicked", ADD_NEW_WINDOW_ID);
+});
+
+addGarmentTypeButton.addEventListener("click", () => {
+  console.log("addnewrenderer - click add garment type button");
+  if (garmentTypeInput.value) {
+    GARMENT_TYPES_TO_SAVE = GARMENT_TYPES_TO_SAVE.add(garmentTypeInput.value);
+    // Clean the input field
+    garmentTypeInput.value = null;
+    // Generate display for the newly added field and button to remove
+    generateGarmentFieldAndRemoveButton(GARMENT_TYPES_TO_SAVE);
+  }
 });
 
 addImagesButton.addEventListener("click", () => {
@@ -41,6 +57,7 @@ submitNewPatternButton.addEventListener("click", () => {
     company: companyNameInput.value,
     year: yearInput.value,
     notes: notesInput.value,
+    garments: Array.from(GARMENT_TYPES_TO_SAVE),
   };
   ipcRenderer.send("submit-new-pattern-button-clicked", pattern);
 });
@@ -67,3 +84,39 @@ ipcRenderer.on("additional-images-uploaded", (event, images) => {
     ADDITIONAL_IMAGES_TO_SAVE
   );
 });
+
+/**
+ * Creates a row for each element in the garment type list and
+ * adds it to the table if it doesn't exist yet.
+ * It also adds an event listener for a remove button for each
+ * entry that when clicked removes the HTML element from the UI
+ * and also the same element from the list.
+ */
+const generateGarmentFieldAndRemoveButton = () => {
+  console.log("garments: " + GARMENT_TYPES_TO_SAVE.size);
+  GARMENT_TYPES_TO_SAVE.forEach((element) => {
+    // If an element hasn't been added yet add it now
+    let rowId = element + "-tr";
+    if (document.getElementById(rowId) === null) {
+      let row = document.createElement("tr");
+      row.setAttribute("id", rowId);
+      let garmentNameCell = document.createElement("td");
+      garmentNameCell.innerText = element;
+      let removeButtonCell = document.createElement("td");
+      let removeButton = document.createElement("button");
+      removeButton.setAttribute("id", element + "-button");
+      removeButton.setAttribute("type", "button");
+      removeButton.innerText = "Remove";
+      removeButton.addEventListener("click", () => {
+        // Remove the HTML for the corresponding row
+        document.getElementById(rowId).remove();
+        // Remove the element from the global array
+        GARMENT_TYPES_TO_SAVE.delete(element);
+      });
+      removeButtonCell.appendChild(removeButton);
+      row.appendChild(garmentNameCell);
+      row.appendChild(removeButtonCell);
+      garmentsAddedDisplay.appendChild(row);
+    }
+  });
+};
--- app/patterndetailrenderer.js
+++ app/patterndetailrenderer.js
@@ -8,6 +8,9 @@ const coverImageDisplay = document.querySelector("#cover-image");
 const changeCoverImageButton = document.querySelector("#change-cover-image");
 const companyNameInput = document.querySelector("#company-name");
 const yearInput = document.querySelector("#year");
+const garmentTypeInput = document.querySelector("#garment");
+const addGarmentTypeButton = document.querySelector("#add-garment-type");
+const garmentsAddedDisplay = document.querySelector("#garments-added");
 const notesInput = document.querySelector("#notes");
 const additionalImagesDisplay = document.querySelector("#additional-images");
 const addImagesButton = document.querySelector("#add-images");
@@ -18,12 +21,24 @@ let PATTERN_FULL_DATA;
 let COVER_IMAGE;
 // At any given time this array contains all the images that can be displayed and saved
 let ADDITIONAL_IMAGES_TO_SAVE = [];
+let GARMENT_TYPES_TO_SAVE = new Set();
 
 /***** Event Listeners *****/
 
 changeCoverImageButton.addEventListener("click", () => {
   console.log("patterndetailrenderer - click change cover image button");
   ipcRenderer.send("open-cover-image-button-clicked", PATTERN_FULL_DATA._id);
+});
+
+addGarmentTypeButton.addEventListener("click", () => {
+  console.log("patterndetailrenderer - click add garment type button");
+  if (garmentTypeInput.value) {
+    GARMENT_TYPES_TO_SAVE = GARMENT_TYPES_TO_SAVE.add(garmentTypeInput.value);
+    // Clean the input field
+    garmentTypeInput.value = null;
+    // Generate display for the newly added field and button to remove
+    generateGarmentFieldAndRemoveButton(GARMENT_TYPES_TO_SAVE);
+  }
 });
 
 addImagesButton.addEventListener("click", () => {
@@ -45,14 +60,6 @@ deletePatternButton.addEventListener("click", () => {
 ipcRenderer.on("pattern-details-ready", (event, pattern) => {
   console.log("patterndetailrenderer - received details: " + pattern.name);
   fillDetailsFromPattern(pattern);
-  uifunctions.displayCover(coverImageDisplay, pattern.cover);
-  if (ADDITIONAL_IMAGES_TO_SAVE.length > 0) {
-    uifunctions.displayAdditionalImages(
-      additionalImagesDisplay,
-      ADDITIONAL_IMAGES_TO_SAVE
-    );
-  }
-  addListenersToImages();
 });
 
 ipcRenderer.on("cover-image-uploaded", (event, cover) => {
@@ -95,11 +102,14 @@ const createPatternForSaving = () => {
     company: companyNameInput.value,
     year: yearInput.value,
     notes: notesInput.value,
+    garments: Array.from(GARMENT_TYPES_TO_SAVE),
   };
 };
 
 /**
- * Fills the UI fields with the provided pattern data
+ * Fills the UI fields with the provided pattern data and dynamically
+ * generates garment type entries and buttons as well as adding listeners
+ * to remove buttons for garment types and to additional images.
  * @param {Object} pattern The pattern to use for displaying data
  */
 const fillDetailsFromPattern = (pattern) => {
@@ -109,6 +119,18 @@ const fillDetailsFromPattern = (pattern) => {
   companyNameInput.value = pattern.company;
   yearInput.value = pattern.year;
   notesInput.value = pattern.notes;
+  GARMENT_TYPES_TO_SAVE = new Set(pattern.garments);
+
+  generateGarmentFieldAndRemoveButton(GARMENT_TYPES_TO_SAVE);
+
+  uifunctions.displayCover(coverImageDisplay, pattern.cover);
+  if (ADDITIONAL_IMAGES_TO_SAVE.length > 0) {
+    uifunctions.displayAdditionalImages(
+      additionalImagesDisplay,
+      ADDITIONAL_IMAGES_TO_SAVE
+    );
+  }
+  addListenersToImages();
 };
 
 /**
@@ -123,5 +145,41 @@ const addListenersToImages = () => {
       let id = element.id.match(/[0-9]+$/);
       ipcRenderer.send("image-area-clicked", PATTERN_FULL_DATA, id);
     });
+  });
+};
+
+/**
+ * Creates a row for each element in the garment type list and
+ * adds it to the table if it doesn't exist yet.
+ * It also adds an event listener for a remove button for each
+ * entry that when clicked removes the HTML element from the UI
+ * and also the same element from the list.
+ */
+const generateGarmentFieldAndRemoveButton = () => {
+  console.log("garments: " + GARMENT_TYPES_TO_SAVE.size);
+  GARMENT_TYPES_TO_SAVE.forEach((element) => {
+    // If an element hasn't been added yet add it now
+    let rowId = element + "-tr";
+    if (document.getElementById(rowId) === null) {
+      let row = document.createElement("tr");
+      row.setAttribute("id", rowId);
+      let garmentNameCell = document.createElement("td");
+      garmentNameCell.innerText = element;
+      let removeButtonCell = document.createElement("td");
+      let removeButton = document.createElement("button");
+      removeButton.setAttribute("id", element + "-button");
+      removeButton.setAttribute("type", "button");
+      removeButton.innerText = "Remove";
+      removeButton.addEventListener("click", () => {
+        // Remove the HTML for the corresponding row
+        document.getElementById(rowId).remove();
+        // Remove the element from the global array
+        GARMENT_TYPES_TO_SAVE.delete(element);
+      });
+      removeButtonCell.appendChild(removeButton);
+      row.appendChild(garmentNameCell);
+      row.appendChild(removeButtonCell);
+      garmentsAddedDisplay.appendChild(row);
+    }
   });
 };
